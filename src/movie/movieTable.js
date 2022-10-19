2889a79bb5a479a1a58da60038fceebc057b37ce
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-19 11:13:36 +0100

    movieTable structure changed

--- src/movie/movieTable.js
+++ src/movie/movieTable.js
@@ -3,6 +3,14 @@ const { sequelize } = require ("../db/connection")
 
 const Movie = sequelize.define("Movie", {
     title: {
-        
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    actor: {
+        type: DataTypes.STRING,
+        defaultValue: "Not Specified"
     }
 })
+
+module.exports = Movie
